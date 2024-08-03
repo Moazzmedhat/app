7bcb2d926733588936015d2b937e02e506d8b818
Moazzmedhat <moazmedhat028@gmail.com>
2024-08-03 23:11:07 +0300

    aye haga

---
+++ moza/script.js
@@ -0,0 +1,28 @@
+  
+  
+  
+let map;
+
+async function initMap() {
+ 
+  const position = { lat: -25.344, lng: 131.031 };
+  
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
